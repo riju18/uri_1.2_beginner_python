c1891d235bd97568e3079bd20a167d63f63b66df
riju18 <rijuchowdhury18@gmail.com>
2020-12-27 10:21:01 +0600

    1048 is accepted.

--- 1048_salary_increase.py
+++ 1048_salary_increase.py
@@ -15,7 +15,7 @@ elif salaryRound >= 400.01 and salaryRound <= 800:
     print('Novo salario: {0:.2f}'.format(Novo_salario))
     print('Reajuste ganho: {0:.2f}'.format(Reajuste_ganho))
     print('Em percentual: {} %'.format(Em_percentual))
-elif salaryRound >= 800.01 and salaryRound <= 1200:
+elif 800.01 <= salaryRound <= 1200:
     Reajuste_ganho = (salaryRound * 10) / 100
     Novo_salario = Reajuste_ganho + salaryRound
     Em_percentual = 10
@@ -36,3 +36,4 @@ elif salaryRound > 2000:
     print('Novo salario: {0:.2f}'.format(Novo_salario))
     print('Reajuste ganho: {0:.2f}'.format(Reajuste_ganho))
     print('Em percentual: {} %'.format(Em_percentual))
+
